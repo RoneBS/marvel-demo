a2193daef237d263a65ba305144d619fb0272faa
Rone Silveira <ronebs10@hotmail.com>
2021-01-07 23:04:57 -0300

    adding section on the Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
+import sectionImg from '../../assets/wolverine.jpeg';
+
 import { Section } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Header />
-      <Section />
+      <Section>
+        <p>Wolverine the imortal hero: new commics from this summer</p>
+        <img src={sectionImg} alt="wolverine" />
+      </Section>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,19 @@
-import { title } from 'process';
 import styled from 'styled-components';
 
 export const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
   width: 100%;
   height: 420px;
+
+  p {
+    font-size: 18px;
+  }
+
+  img {
+    width: 100%;
+    height: 420px;
+  }
 `;
