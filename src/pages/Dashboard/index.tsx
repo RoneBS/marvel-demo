3dbb00879305e7cf236f8cbc8041282562bfcf9a
Rone Silveira <ronebs10@hotmail.com>
2021-01-08 16:28:28 -0300

    adding card section

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,17 +3,38 @@ import React from 'react';
 import Header from '../../components/Header';
 
 import sectionImg from '../../assets/wolverine.jpeg';
+import wolvImg from '../../assets/wolverine-card.jpeg';
+import cicImg from '../../assets/ciclope-card.jpeg';
+import magImg from '../../assets/magneto-card.jpeg';
 
-import { Section } from './styles';
+import { Section, Title, Card } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Header />
       <Section>
-        <p>Wolverine the imortal hero: new commics from this summer</p>
+        <p>
+          Wolverine the imortal hero:
+          <br />
+          new commics from this summer
+        </p>
         <img src={sectionImg} alt="wolverine" />
       </Section>
+      <Title>
+        <h1>Choose your Hero</h1>
+      </Title>
+      <Card>
+        <div>
+          <img src={wolvImg} alt="" />
+        </div>
+        <div>
+          <img src={cicImg} alt="" />
+        </div>
+        <div>
+          <img src={magImg} alt="" />
+        </div>
+      </Card>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,19 +1,55 @@
 import styled from 'styled-components';
 
 export const Section = styled.div`
+  width: 100%;
+  height: 420px;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 40px;
-  width: 100%;
-  height: 420px;
 
   p {
-    font-size: 18px;
+    color: white;
+    font-size: 24px;
+    position: absolute;
+    padding-right: 600px;
   }
 
   img {
     width: 100%;
     height: 420px;
+  }
+`;
+
+export const Title = styled.h1`
+  display: flex;
+  margin-top: 40px;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: 30px;
+    color: black;
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  div {
+    display: flex;
+    justify-content: center;
+    width: 350px;
+    height: 400px;
+    margin: 50px;
+    border-radius: 40px;
+    box-shadow: 2px 2px 22px rgba(0, 0, 0, 0.2);
+    background-color: grey;
+
+    img {
+      width: 450px;
+      height: 250px;
+      border-radius: 40px;
+      background-color: white;
+    }
   }
 `;
