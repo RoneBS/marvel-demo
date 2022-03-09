ae1e46b1c593e0788254f1bdae53e3c0b7a7bca0
Rone Silveira <ronebs10@hotmail.com>
2022-03-09 13:32:28 -0300

    adding global volors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+:root {
+  --black: #000000;
+  --white: #fff;
+  --gray: #808080;
+  --deep-gray: #333333;
+  --red: #ff0000;
+}
+
   * {
     margin: 0;
     padding: 0;
